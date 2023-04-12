f82db0e33d1bed13741a21d2b15e6e2c0c707a5d
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-12 16:41:24 +0200

    PHP get data from DB and print on view "Welcome"

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    // Metodo che ritorna la Vista "Welcome"
+    // Metodo che ritorna la Vista "Welcome" e passa i dati recuperati dal Database
     public function homepage()
     {
-        return view('welcome');
+        $trains = Train::all();
+        // dd($trains);
+
+        $data = [
+            'trains' => $trains
+        ];
+
+        return view('welcome', $data);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,29 @@
 @section('content')
 
 <main>
+    <div class="container text-center">
+        <h1>Trains</h1>
+    </div>
+
     <div class="container">
-        TRAINS
+        <div class="row row-cols-4">
+
+            @foreach ($trains as $train)
+            <div class="col">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">{{$train->code}}</h5>
+                        <p class="card-text">{{$train->company}}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{{$train->departure_station}} - {{$train->arrival_station}}</li>
+                        <li class="list-group-item">{{$train->departure_time}} - {{$train->arrival_time}}</li>
+                    </ul>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
     </div>
 </main>
 
